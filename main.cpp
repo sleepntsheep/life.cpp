c4ca68e031f476f56851ef5c81633c35a0c11dc0
sleepntsheep <a@papangkorn.com>
2022-01-09 15:30:50 +0700

    GPS fix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 #include <set>
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
@@ -9,7 +11,6 @@
 #include <SDL2/SDL_ttf.h>
 #endif
 #include "main.h"
-#include <vector>
 
 SDL_Renderer *renderer;
 SDL_Window *window;
@@ -23,13 +24,16 @@ bool running = true;
 double delta;
 std::string game_state = "setup";
 
+int cell_size = CELL_SIZE;
 static Uint32 next_time;
-int posX = CENTER_X / CELL_SIZE;
-int posY = CENTER_Y / CELL_SIZE;
-int GPS = GenPS;
-int gen = 0;
+int posX = CENTER_X / cell_size;
+int posY = CENTER_Y / cell_size;
+unsigned int GPS = GenPS;
+unsigned int gen = 0;
 int width = WIDTH;
 int height = HEIGHT;
+unsigned int gen_passed = 0;
+bool border = 1;
 
 std::set<std::pair<int, int>> keys;
 
@@ -53,11 +57,13 @@ std::vector<std::pair<int, int>> get_neighbors(int x, int y) {
 }
 
 void next_gen() {
+    if (gen_passed == GPS)
+        return;
     std::set<std::pair<int, int>> new_keys = keys;
-    for (auto cell : keys) {
+    for (std::pair<int, int> cell : keys) {
         std::vector<std::pair<int, int>> neighbors = get_neighbors(cell.first, cell.second);
         int alive_neighbors = 0;
-        for (auto neighbor : neighbors) {
+        for (std::pair<int, int> neighbor : neighbors) {
             if (keys.find(neighbor) != keys.end()){
                 alive_neighbors++;
                 continue;
@@ -75,12 +81,12 @@ void next_gen() {
     }
     keys = new_keys;
     gen++;
+    gen_passed = 0;
 };
 
 void write(std::string str, int x, int y) {
     SDL_Surface *surface;
     SDL_Texture *texture;
-//    surface = TTF_RenderText_Blended(font, ("Gen: " + std::to_string(gen) + "\nMode: " + game_state + "\nX, Y: " + std::to_string(posX) + ", " + std::to_string(posY)).c_str(), Green);
     surface = TTF_RenderText_Blended(font, str.c_str(), Green);
     texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Rect message_rect = { x, y, surface->w, surface->h };
@@ -88,8 +94,6 @@ void write(std::string str, int x, int y) {
     SDL_RenderCopy(renderer, texture, NULL, &message_rect);
     SDL_DestroyTexture(texture);
 }
-
-
 
 void input() {
     SDL_Event event;
@@ -119,12 +123,27 @@ void input() {
                     case SDLK_l:
                         posX--;
                         break;
+                    case SDLK_b:
+                        border = !border;
+                        break;
                     case SDLK_EQUALS:
                         GPS++;
                         break;
                     case SDLK_MINUS:
                         if (GPS > 1)
                             GPS--;
+                        break;
+                    case SDLK_r:
+                        keys.clear();
+                        gen = 0;
+                        game_state = "setup";
+                        break;
+                    case SDLK_1:
+                        if (cell_size > 1)
+                            cell_size--;
+                        break;
+                    case SDLK_2:
+                        cell_size++;
                         break;
                 }
                 break;
@@ -133,8 +152,8 @@ void input() {
                 SDL_GetMouseState(&x, &y);
                 if (game_state == "setup") {
                     std::pair<int, int> position;
-                    position.first = x / CELL_SIZE - posX;
-                    position.second = y / CELL_SIZE - posY;
+                    position.first = x / cell_size - posX;
+                    position.second = y / cell_size - posY;
                     if (keys.find(position) == keys.end())
                         keys.insert(position);
                     else
@@ -157,19 +176,21 @@ void render() {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
 
-    for (int i = CELL_SIZE; i < width; i += CELL_SIZE) {
-        SDL_Rect r = {i, 0, BORDER_SIZE, height};
-        SDL_RenderFillRect(renderer, &r);
-    }
+    if (border) {
+        for (int i = cell_size; i < width; i += cell_size) {
+            SDL_Rect r = {i, 0, BORDER_SIZE, height};
+            SDL_RenderFillRect(renderer, &r);
+        }
 
-    for (int i = CELL_SIZE; i < height; i += CELL_SIZE) {
-        SDL_Rect r = {0, i, width, BORDER_SIZE};
-        SDL_RenderFillRect(renderer, &r);
+        for (int i = cell_size; i < height; i += cell_size) {
+            SDL_Rect r = {0, i, width, BORDER_SIZE};
+            SDL_RenderFillRect(renderer, &r);
+        }
     }
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     for (auto cell : keys) {
-        SDL_Rect r = { (posX + cell.first) * CELL_SIZE, (posY + cell.second) * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+        SDL_Rect r = { (posX + cell.first) * cell_size, (posY + cell.second) * cell_size, cell_size, cell_size};
         SDL_RenderFillRect(renderer, &r);
     }
 
@@ -193,9 +214,25 @@ void render() {
     SDL_RenderPresent(renderer);
 }
 
-void update() {
-    if ((frames % (FPS / GPS)) == 0 && game_state == "main_game")
-        next_gen();
+void update()
+{
+    if (game_state != "main_game")
+        return;
+    if (frames % FPS == 0)
+        gen_passed = 0;
+    if (GPS <= 60){
+        if ((frames % (FPS / GPS)) == 0)
+            next_gen();
+    }
+    else {
+        for (int i = 0; i < (int)GPS/FPS; i++)
+            next_gen();
+        if (frames % ((int)((float)GPS / (float)FPS) + 1) == 0)
+            next_gen();
+    }
+    //if (frames % FPS == FPS - 1 && gen_passed < GPS)
+    //    while (gen_passed < GPS)
+    //        next_gen();
 }
 
 void main_loop() {
@@ -224,7 +261,7 @@ int main() {
 
     SDL_SetWindowTitle(window, "Conway's Game of Life");
 
-    std::cout << "Click on cell to toggle\nPress space to switch between setup/main mode\nPress - + to change GPS (gen per sec)\nUse h, j, k, l to navigate around (vim keybind)" << std::endl;
+    std::cout << "Click on cell to toggle\nPress space to switch between setup/main mode\nPress - + to change GPS (gen per sec)\nUse h, j, k, l to navigate around (vim keybind)\npress 1, 2 to change cell size" << std::endl;
 
     #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(main_loop, 0, 1);
